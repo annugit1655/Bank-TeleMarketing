92df99e9f150a8edca208a5bf3b9bfc7faf670cb
ayannu <ayannu1655@gmail.com>
2025-07-05 18:33:16 +0530

    'FirstCommit'

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a machine learning project to optimize marketing strategies for bank term deposits.',
+    author='ay_annu',
+    license='',
+)
