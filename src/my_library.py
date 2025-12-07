2de38386965b85d8e8c335d5900a286d3ea28f73
ayannu <ayannu1655@gmail.com>
2025-12-07 19:35:16 +0530

    'inital_imports'

--- src/my_library.py
+++ src/my_library.py
@@ -1,7 +1,17 @@
-import pandas as pd 
-import numpy as np  
-import sidetable
-import sklearn
-import scipy.stats as stats
-import matplotlib.pyplot as plt
-import seaborn as sns
+
+# helper function that set up inital imports
+def make_inital_imports():
+    import pandas as pd
+    import numpy as np
+    import scipy.stats as stats
+    import matplotlib.pyplot as plt
+    import matplotlib.ticker as ticker
+    import seaborn as sns
+    
+    return pd, np, stats, plt, ticker
+
+# helper function to load a dataset
+def load_data(url):
+    # Load CSV file with ; seperator
+    df = pd.read_csv(url, sep=';')
+    return df
