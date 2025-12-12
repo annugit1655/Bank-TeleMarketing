458673129a8bb8169449fe8b33e84dfe5a7c9454
ayannu <ayannu1655@gmail.com>
2025-12-12 14:35:36 +0530

    'add_files'

--- src/my_library.py
+++ src/my_library.py
@@ -1,13 +1,14 @@
+import pandas as pd
+import numpy as np
+#import scipy.stats as stats
+import matplotlib.pyplot as plt
+#import matplotlib.ticker as ticker
+import seaborn as sns
+
+#global pd, np, plt, sns
 
 # helper function that set up inital imports
 def make_inital_imports():
-    import pandas as pd
-    import numpy as np
-    #import scipy.stats as stats
-    import matplotlib.pyplot as plt
-    #import matplotlib.ticker as ticker
-    import seaborn as sns
-    
     return pd, np, plt, sns
 
 # helper function to load a dataset
